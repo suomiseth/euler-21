b5b61a9a55ae3efe9e84346a14e6bb33ecde06af
suomiseth <suomiseth@gmail.com>
2015-11-22 09:20:31 -0500

    75% of the way there

--- first-try.rb
+++ first-try.rb
@@ -4,21 +4,29 @@
 
 class AmicableNumbersSum
 
-
   def initialize(limt)
     @limit = limit
   end  
-
-  def find_amicable_nums
-    
-  end
 
   def sum
     find_amicable_nums.inject(:+)
   end
 
+  def find_amicable_nums
+    (1..@limit).inject({}) {|hash, n| hash[n] = divisor_sum(n)}
+  end
+
+  def divisor_sum(n)
+    (1..(n / 2)).select {|x| n % x == 0}.inject{:+}
+  end
 end
 
+
+# brute force method
+# go through each number to its limit and find the sum of divisors
+# hash that number to the sum of its divisors
+# see if there are any matches by iterating through (find dups)
+# sum up the original numbers of 
 
 puts "what number do you want to check amicable numbers to?"
 limit = gets.strip
